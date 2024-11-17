a5bed2eb91e9057f23da69e6c2bfc2d72ee7e68e
Keshav Garg <98670495+KeshavG69@users.noreply.github.com>
2024-11-18 03:12:52 +0530

    Add files via upload

---
+++ helper.py
@@ -0,0 +1,154 @@
+import pandas as pd
+import numpy as np
+from langchain.retrievers.multi_vector import MultiVectorRetriever
+import uuid
+from typing import List
+
+from typing_extensions import TypedDict
+from langgraph.graph import END, StateGraph, START
+from pydantic import BaseModel, Field
+from typing import List
+import cohere
+from langchain_core.documents import Document
+from langchain_core.runnables import RunnablePassthrough, RunnableLambda
+from dotenv import load_dotenv
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+import os
+from langchain_cohere import CohereEmbeddings
+from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+import json
+from langchain.retrievers.contextual_compression import ContextualCompressionRetriever
+from langchain_cohere import CohereRerank
+from langchain_community.llms import Cohere
+from langchain_together import ChatTogether
+from langchain_core.prompts import ChatPromptTemplate, FewShotChatMessagePromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_groq import ChatGroq
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_chroma import Chroma
+from langchain.storage import InMemoryByteStore
+
+load_dotenv()
+
+llm = ChatGroq(
+    temperature=0.1,
+    groq_api_key=os.getenv("GROQ_API_KEY"),
+    model_name="llama-3.1-70b-versatile",
+    streaming=True,
+)
+llm90 = ChatGroq(
+    temperature=0.1,
+    groq_api_key=os.getenv("GROQ_API_KEY"),
+    model_name="llama-3.2-90b-vision-preview",
+    streaming=True,
+)
+
+llm_together = ChatTogether(
+    model="meta-llama/Meta-Llama-3.1-70B-Instruct-Turbo",
+    temperature=0.1,
+    api_key=os.getenv("TOGETHER_API_KEY"),
+    streaming=True,
+)
+
+
+co = cohere.Client(os.getenv("COHERE_API_KEY"))
+
+embeddings = CohereEmbeddings(
+    model="embed-english-v3.0",
+    cohere_api_key=os.getenv("COHERE_API_KEY"),
+)
+
+embd = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+
+
+def summary_llm(articles, links, headings, path, df):
+    summaries = []
+    template = """"
+<Introduction>
+You are an expert summarizer trained to distill large pieces of information into concise, precise, and retrieval-optimized summaries. Your task is to generate a structured and contextually relevant summary of the given article. This summary will be used in a vector database for similarity search to answer customer care queries.
+</Introduction>
+
+<Rules>
+The summary should:
+
+Highlight the main topic or purpose of the article in one sentence.
+Identify key points, features, or solutions addressed in the article.
+Provide any relevant details that distinguish the article from others in similar domains.
+Avoid unnecessary jargon or excessive details—focus on actionable information.
+</Rules>
+
+<Document>
+Article:{doc}
+</Document>
+
+"""
+    summarise_chain = (
+        ChatPromptTemplate.from_template(template) | llm | StrOutputParser()
+    )
+
+    for i, article in enumerate(articles):
+
+        summaries.append(summarise_chain.invoke({"doc": article}))
+    cleaned_summaries = [text.lstrip("**Summary:**\n") for text in summaries]
+
+    doc_ids = [str(uuid.uuid4()) for _ in articles]
+    summary_docs = [
+        Document(page_content=s, metadata={"doc_id": doc_ids[i]})
+        for i, s in enumerate(cleaned_summaries)
+    ]
+    articles_docs = [
+        Document(
+            page_content=article,
+            metadata={"source": links[i], "doc_id": doc_ids[i], "heading": headings[i]},
+        )
+        for i, article in enumerate(articles)
+    ]
+    summary_documents = [
+        {"page_content": doc.page_content, "metadata": doc.metadata}
+        for doc in summary_docs
+    ]
+    articles_documents = [
+        {"page_content": doc.page_content, "metadata": doc.metadata}
+        for doc in articles_docs
+    ]
+
+    df["Sub Article Summaries"] = cleaned_summaries
+    with open("summary.json", "w") as file:
+        json.dump(summary_documents, file, indent=4)
+    with open("articles.json", "w") as file:
+        json.dump(articles_documents, file, indent=4)
+
+    df.to_csv(path, index=False)
+
+
+def compute_cosine_similarity(v1, v2):
+
+    dot_product = np.dot(v1, v2)
+
+    norm_v1 = np.linalg.norm(v1)
+    norm_v2 = np.linalg.norm(v2)
+
+    similarity = dot_product / (norm_v1 * norm_v2)
+
+    return similarity
+
+
+def compute_overall_similarity(response, kb_chunks):
+    """
+    Compute the overall similarity (average) between the response (LLM's output) and KB chunks.
+    This checks how much the response is grounded in the KB content.
+    """
+
+    response_embedding = embeddings.embed_query(response)
+
+    similarities = []
+    for chunk in kb_chunks:
+        chunk_embedding = embeddings.embed_query(chunk.page_content)
+
+        similarity = compute_cosine_similarity(response_embedding, chunk_embedding)
+        similarities.append(similarity)
+
+    overall_similarity = np.mean(similarities)
+
+    return overall_similarity
---
+++ main.py
@@ -0,0 +1,409 @@
+from helper import *
+
+
+path='DoorDashArticleSummary.csv'
+
+
+
+df=pd.read_csv(path)
+
+with open("summary.json", "r") as file:
+    loaded_documents = json.load(file)
+
+
+summary_docs = [
+    Document(page_content=doc["page_content"], metadata=doc["metadata"])
+    for doc in loaded_documents
+]
+
+
+with open("articles.json", "r") as file:
+    loaded_documents = json.load(file)
+
+articles_docs = [
+    Document(page_content=doc["page_content"], metadata=doc["metadata"])
+    for doc in loaded_documents
+]
+
+headings=df['Sub Article Headings'].tolist()
+links=df['Sub Article Links'].tolist()
+articles=df['Sub Article Text'].tolist()
+summary=df['Sub Article Summaries'].tolist()
+
+
+
+vectorstore = Chroma(collection_name="doordash",
+                     embedding_function=embd,
+                     persist_directory="database")
+
+summarise_query_template='''
+Summarize the following chat conversation between a DoorDash Dasher and customer support, and extract a brief issue description that captures the key problem or request. The summary should be specific and actionable, so that it can be used to search for relevant articles in a knowledge base or vector database. Avoid unnecessary details, focusing only on the essential information for article retrieval.
+Do NOT answer the question, just reformulate it if needed and otherwise return it as is.
+Only return Summarised question and nothing else
+Dont write Summary: 
+only return the question and nothing else
+Chat conversation:
+{chat_history}
+
+'''
+
+summarise_query_prompt=ChatPromptTemplate.from_messages([
+    ("system",summarise_query_template),
+    MessagesPlaceholder(variable_name="chat_history"),
+    ("human", "{question}")
+])
+summarise_chain=summarise_query_prompt|llm|StrOutputParser()
+
+
+
+text_splitter = RecursiveCharacterTextSplitter(
+            separators=[
+                "\n\n",
+                "\n",
+
+            ],
+            chunk_size=1000,
+            chunk_overlap=300,
+        )
+
+
+
+
+
+class Groundness(BaseModel):
+  """ Binary Score for groundness check on retrieved documents and the response"""
+  binary_score:str=Field(
+      ...,
+      description='The response by LLm  are grounded in the respect to the retrieved document "yes" or "no"'
+  )
+
+
+ground_structured_llm=llm90.with_structured_output(Groundness)
+
+system = """You are a grader assessing groundness of a LLM response to retrieved documents. \n
+    If the response has grounded facts based on the retrived document grade it as grounded\n
+    Give a binary score 'yes' or 'no' score to indicate whether the response is grounded to the retreived document."""
+
+ground_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", system),
+        ("human", "Retrieved document: \n\n {document} \n\n LLM Response: {llm_response}"),
+    ]
+)
+
+ground_chain=ground_prompt | ground_structured_llm
+
+class Coherence(BaseModel):
+  """ Binary Score for Coherence check on retrieved documents and the response"""
+  binary_score:str=Field(
+      ...,
+      description='The response by LLm  are Coherenent in the respect to the retrieved document "yes" or "no"'
+  )
+
+
+structured_llm_coherent=llm90.with_structured_output(Coherence)
+
+system = """You are a grader assessing coherence of a LLM response to retrieved documents. \n
+    If the response is coherent based on the retrived document grade it as coherent\n
+    Give a binary score 'yes' or 'no' score to indicate whether the response is coherent to the retreived document."""
+coherent_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", system),
+        ("human", "Retrieved document: \n\n {document} \n\n LLM Response: {llm_response}"),
+    ]
+)
+
+coherent_chain=coherent_prompt | structured_llm_coherent
+
+class Compliance(BaseModel):
+  """ Binary Score for Compliance of the LLM Response such that it does not have any harmful lamguage"""
+  binary_score:str=Field(
+      ...,
+      description='The response by LLm  are compliant  such that it does not have any harmful lamguage "yes" or "no"'
+  )
+
+
+structured_llm_compliance=llm90.with_structured_output(Compliance)
+
+
+system = """You are a grader assessing Compliance of a LLM response . \n
+   The response by LLm  are compliant and  does not have any harmful lamguage grade it as compliant\n
+    Give a binary score 'yes' or 'no' score to indicate whether the response is comlpiant and does not have any harmful lamguage."""
+compliant_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", system),
+        ("human", "LLM Response: {llm_response}"),
+    ]
+)
+
+compliant_chain=compliant_prompt | structured_llm_compliance
+
+class Answer(BaseModel):
+  """ Binary Score for if the LLM Response answers the user query"""
+  binary_score:str=Field(
+      ...,
+      description='The response by LLm  answer the user query "yes" or "no"'
+  )
+
+
+structured_llm_answer=llm90.with_structured_output(Answer)
+
+system = """You are a grader assessing Compliance of a LLM response . \n
+   The response by LLm  answers the user query\n
+    Give a binary score 'yes' or 'no' score to indicate whether the response answers the users query properly."""
+answer_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", system),
+        ("human", "LLM Response: {llm_response}"),
+    ]
+)
+
+answer_chain=answer_prompt | structured_llm_answer
+
+class GraphState(TypedDict):
+    """
+    Represents the state of our graph.
+
+    Attributes:
+        question: question
+        generation: LLM generation
+        web_search: whether to add search
+        documents: list of documents
+    """
+
+    query: str
+    generation: str
+    similarity_search: str
+    groundness: str
+    coherence: str
+    compliance: str
+    answer:str
+    chat_history:List
+
+    documents: List[Document]
+
+def generate(state):
+  gen_template=""""
+<Introduction>
+You are a highly intelligent and context-aware assistant specializing in generating precise and helpful answers. Your task is to use the provided article as the primary source of information and the user query to craft an accurate, clear, and concise response.
+</Introduction>
+
+<Rules>
+1. Read and understand the provided article to extract relevant information.
+2. Focus specifically on addressing the user's query.
+3. Provide a solution, explanation, or relevant details derived from the article.
+4. Use simple, user-friendly language and avoid unnecessary repetition.
+5. If the article doesn’t directly address the query, use the information to provide the best alternative or related guidance.
+</Rules>
+
+<Input>
+- **Article:** {docs}
+- ** Query:** {query}
+</Input>
+
+<Output>
+[Provide a detailed and relevant answer tailored to the query based on the article.]
+</Output>
+
+
+"""
+  query=state['query']
+  context=[]
+  context_without_link=[]
+  summarised_query=summarise_chain.invoke({'chat_history':chat_history,'question':query})
+
+  relevant_documents=vectorstore.similarity_search(summarised_query,k=7)
+  keys = [doc.metadata['doc_id'] for doc in relevant_documents]
+  for doc in articles_docs:
+
+
+    if doc.metadata['doc_id'] in keys:
+      context.append(doc)
+      context_without_link.append(doc.page_content)
+
+  response = co.rerank(
+    query=query,
+    documents=context_without_link,
+    top_n=3,
+    model='rerank-english-v3.0'
+)
+  top_docs = [context_without_link[result.index] for result in response.results]
+  top_docs_with_link=[context[result.index] for result in response.results]
+
+  prompt=ChatPromptTemplate.from_template(gen_template)
+  gen_chain={'docs':lambda x: top_docs,'query':RunnablePassthrough()}|prompt|llm_together|StrOutputParser()
+  generation=gen_chain.invoke({'query':summarised_query})
+  chat_history.append(HumanMessage(content=query))
+  chat_history.append(SystemMessage(content=generation))
+  print(summarised_query)
+  return {'documents':top_docs_with_link,'question':query,'generation':generation,'chat_history':chat_history}
+
+
+
+def sim_search(state):
+  documents=state['documents']
+  query=state['query']
+  generation=state['generation']
+
+  splits=text_splitter.split_documents(documents)
+  overall_similarity = compute_overall_similarity(generation, splits)
+  if overall_similarity>0.70:
+    print('Similarity Search Passed')
+    return {'documents':documents,'question':query,'generation':generation,'similarity_search':'yes'}
+  else:
+    print('Similarity Search Not Passed')
+    return {'documents':documents,'question':query,'generation':generation,'similarity_search':'no'}
+
+
+
+
+
+
+def groundness(state):
+  documents=state['documents']
+  query=state['query']
+  generation=state['generation']
+  groundness_chain=ground_chain.invoke({'document':documents,'llm_response':generation})
+  if groundness_chain.binary_score=='yes':
+    print('Groundness Passed')
+    return {'documents':documents,'question':query,'generation':generation,'groundness':'yes','similarity_search':'no'}
+  else:
+    print('Groundness Not Passed')
+    return {'documents':documents,'question':query,'generation':generation,'groundness':'no','similarity_search':'no'}
+
+def coherence(state):
+  documents=state['documents']
+  query=state['query']
+  generation=state['generation']
+  coherence_chain=coherent_chain.invoke({'document':documents,'llm_response':generation})
+  if coherence_chain.binary_score=='yes':
+    print('Coherence Passed')
+    return {'documents':documents,'question':query,'generation':generation,'groundness':'yes','coherence':'yes','similarity_search':'no'}
+  else:
+    print('Coherence Not Passed')
+    return {'documents':documents,'question':query,'generation':generation,'groundness':'yes','coherence':'no','similarity_search':'no'}
+
+def compliance(state):
+  documents=state['documents']
+  query=state['query']
+  generation=state['generation']
+  compliance_chain=compliant_chain.invoke({'llm_response':generation})
+  if compliance_chain.binary_score=='yes':
+    print('Compliance Passed')
+    return {'documents':documents,'question':query,'generation':generation,'groundness':'yes','coherence':'yes','compliance':'yes','similarity_search':'no'}
+  else:
+    print('Compliance Not Passed')
+    return {'documents':documents,'question':query,'generation':generation,'groundness':'yes','coherence':'yes','compliance':'no','similarity_search':'no'}
+
+def answer(state):
+  documents=state['documents']
+  query=state['query']
+  generation=state['generation']
+
+  answer_chain=answer_chain.invoke({'llm_response':generation,'query':query})
+
+  if answer_chain.binary_score=='yes':
+    print('Answer Passed')
+    return {'documents':documents,'question':query,'generation':generation,'groundness':'yes','coherence':'yes','compliance':'yes','similarity_search':'no','answer':'yes'}
+  else:
+    print('Answer Not Passed')
+    return {'documents':documents,'question':query,'generation':generation,'groundness':'yes','coherence':'yes','compliance':'yes','similarity_search':'no','answer':'no'}
+
+
+def send_user(state):
+  documents=state['documents']
+  query=state['query']
+  generation=state['generation']
+  print('Answer Sent to User')
+  print(generation)
+
+def human_support(state):
+  documents=state['documents']
+  query=state['query']
+  generation=state['generation']
+  print('Human Support')
+
+
+def sim_edge(state):
+  if state['similarity_search']=='yes':
+    print('Send User')
+    return 'send_user'
+  print('Groundness')
+  return 'groundness'
+
+def ground_edge(state):
+  if state['groundness']=='yes':
+    print('Coherence')
+    return 'coherence'
+  print('Human Support')
+  return 'human_support'
+
+def coherence_edge(state):
+  if state['coherence']=='yes':
+    print('Compliance')
+    return 'compliance'
+  print('Human Support')
+  return 'human_support'
+
+def compliance_edge(state):
+  if state['compliance']=='yes':
+    print('Answer')
+    return 'answer'
+  print('Human Support')
+  return 'human_support'
+
+def answer_edge(state):
+  if state['answer']=='yes':
+    print('Send User')
+    return 'send_user'
+  print('Human Support')
+  return 'human_support'
+
+ 
+workflow = StateGraph(GraphState)
+workflow.add_node('generate',generate)
+workflow.add_node('sim_search',sim_search)
+workflow.add_node('groundedness',groundness)
+workflow.add_node('coherences',coherence)
+workflow.add_node('compliances',compliance)
+workflow.add_node('answers',answer)
+workflow.add_node('send_user',send_user)
+workflow.add_node('human_support',human_support)
+workflow.add_edge(START, 'generate')
+workflow.add_edge('generate', 'sim_search')
+workflow.add_conditional_edges('sim_search',sim_edge,
+                               {
+                                   "groundness":'groundedness',
+                                   "send_user":'send_user',
+
+                               })
+workflow.add_conditional_edges('groundedness',ground_edge,
+                               {
+                                   "coherence":'coherences',
+                                   "human_support":'human_support',
+                               })
+workflow.add_conditional_edges('coherences',coherence_edge,
+                               {
+                                   "compliance":'compliances',
+                                   "human_support":'human_support',
+                               })
+workflow.add_conditional_edges('compliances',compliance_edge,
+                               {
+                                   "answer":'answers',
+                                   "human_support":'human_support',
+                               })
+workflow.add_conditional_edges('answers',answer_edge,
+                               {
+                                   "send_user":'send_user',
+                                   "human_support":'human_support',
+                               })
+
+workflow.add_edge("send_user", END)
+app = workflow.compile()
+
+chat_history=[]
+while True:
+  query=input('Input Query: ')
+  if query.lower()=='quit':
+    break
+  app.invoke ({'query':query})
