ce515053e8f885d4531b862368044828c2002e10
Keshav Garg <98670495+KeshavG69@users.noreply.github.com>
2024-11-21 10:36:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 import streamlit as st
 from main import *
-
+from hugchat import hugchat
+from hugchat.login import Login
 
 st.set_page_config(page_title="🤗💬 HugChat")
 
 if "messages" not in st.session_state.keys():
+    
     st.session_state.messages = [{"role": "assistant", "content": "How may I help you?"}]
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = []
+    
 
+    
 
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
@@ -16,15 +22,16 @@ for message in st.session_state.messages:
     
 if prompt := st.chat_input("Send a message"):
     st.session_state.messages.append({"role": "user", "content": prompt})
+    st.session_state.chat_history.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.write(prompt)
-        
+
 
 if st.session_state.messages[-1]["role"] != "assistant":
     with st.chat_message("assistant"):
         with st.spinner("Thinking..."):
-            chat_history=st.session_state.messages
-            response = app.invoke ({'query':prompt,'chat_history':st.session_state.messages})
+            
+            response = app.invoke ({'query':prompt,'chat_history':st.session_state.chat_history})
             st.markdown(response['generation']) 
     message = {"role": "assistant", "content": response['generation']}
     st.session_state.messages.append(message)
